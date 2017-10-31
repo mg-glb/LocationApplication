6c62e3256f4d897d3e2a95f70fba862cc523264b
Maximiliano Gigena <m.gigena@globant.com>
2017-10-30 23:19:00 -0300

    refactoring method

--- app/src/main/java/com/example/mgigena/locationapplication/MainActivity.java
+++ app/src/main/java/com/example/mgigena/locationapplication/MainActivity.java
@@ -98,18 +98,12 @@ public class MainActivity extends AppCompatActivity
     }
 
     protected void locationOperations(String mode) {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        } else {
+        boolean condition = ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED;
+        condition &= ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED;
+        if (!condition) {
             switch (mode) {
                 case "start":
                     createLocationRequest();
